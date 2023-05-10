798d69f37c1561ba7eade3e6a9464dd56eeb592e
Serhii Tomashevskyi <tom5423000@gmail.com>
2023-05-10 22:30:33 +0300

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
 import ImageGalleryItem from './ImageGalleryItem';
 import Button from './Button';
 import Loader from './Loader/Loader';
 import Modal from './Modal/Modal';
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const MY_API_PIXABAY_KEY = '?key=34337026-7de7d7fed724711432526467d';
+const quantityObjects = 32;
 
 export default function App() {
   const [valueInput, setValueInput] = useState('');
@@ -15,25 +20,58 @@ export default function App() {
   const [modalImage, setModalImage] = useState('');
   const [upLoadDataTotalhits, setUpLoadDataTotalhits] = useState('');
 
+  useEffect(() => {
+    if (valueInput === '') {
+      return;
+    }
+    setLoading(true);
+    if (page !== 1) {
+      window.scrollBy({
+        top: 400,
+        behavior: 'smooth',
+      });
+    } else {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+
+    response();
+    async function response() {
+      try {
+        const response = await axios.get(
+          `${BASE_URL}${MY_API_PIXABAY_KEY}&q=${valueInput}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${quantityObjects}`
+        );
+        const updateHits = response.data.hits.map(e => {
+          return {
+            id: e.id,
+            tags: e.tags,
+            previewURL: e.previewURL,
+            largeImageURL: e.largeImageURL,
+          };
+        });
+        if (page === 1) {
+          setUpLoadImages(updateHits);
+        } else {
+          setUpLoadImages(prevUpLoadImages => [
+            ...prevUpLoadImages,
+            ...updateHits,
+          ]);
+        }
+        setUpLoadDataTotalhits(response.data.totalHits);
+      } catch (error) {
+        return error;
+      } finally {
+        setLoading(false);
+      }
+    }
+  }, [page, valueInput]);
+
   const formSubmit = e => {
-    setValueInput(e);
     setPage(1);
+    setValueInput(e);
   };
 
   const incrementPage = () => {
     setPage(prevPage => prevPage + 1);
-  };
-
-  const loadingStatus = e => {
-    setLoading(e);
-  };
-
-  const handleUpLoadImages = e => {
-    setUpLoadImages(e);
-  };
-
-  const handleUpLoadDataTotalhits = e => {
-    setUpLoadDataTotalhits(e);
   };
 
   const openModal = event => {
@@ -59,11 +97,7 @@ export default function App() {
       <Searchbar onSubmit={formSubmit}></Searchbar>
       <ImageGallery>
         <ImageGalleryItem
-          valueInput={valueInput}
-          currentPage={page}
-          loading={loadingStatus}
-          handleUpLoadImages={handleUpLoadImages}
-          handleUpLoadDataTotalhits={handleUpLoadDataTotalhits}
+          upLoadImages={upLoadImages}
           openModal={openModal}
         ></ImageGalleryItem>
       </ImageGallery>
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,104 +1,10 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useState, useEffect, useRef } from 'react';
-import { React } from 'react';
-import axios from 'axios';
 import PropTypes from 'prop-types';
 import css from './ImageGalleryItem.module.css';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const MY_API_PIXABAY_KEY = '?key=34337026-7de7d7fed724711432526467d';
-const quantityObjects = 32;
-
-function usePrevious(value) {
-  const ref = useRef();
-  useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
-}
-
-export default function ImageGalleryItem(props) {
-  const [upLoadImages, setUpLoadImages] = useState([]);
-  const prevProps = usePrevious(props);
-
-  useEffect(() => {
-    response();
-    async function response() {
-      props.loading(true);
-      try {
-        const response = await axios.get(
-          `${BASE_URL}${MY_API_PIXABAY_KEY}&q=${props.valueInput}&image_type=photo&orientation=horizontal&safesearch=true&page=${props.currentPage}&per_page=${quantityObjects}`
-        );
-
-        const updateHits = response.data.hits.map(e => {
-          return {
-            id: e.id,
-            tags: e.tags,
-            previewURL: e.previewURL,
-            largeImageURL: e.largeImageURL,
-          };
-        });
-
-        setUpLoadImages(updateHits);
-        props.handleUpLoadDataTotalhits(response.data.totalHits);
-        props.handleUpLoadImages(updateHits);
-      } catch (error) {
-        return error;
-      } finally {
-        props.loading(false);
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    if (props.currentPage === 1 && props.valueInput === '') {
-      return;
-    }
-    if (props.currentPage !== 1) {
-      window.scrollBy({
-        top: 400,
-        behavior: 'smooth',
-      });
-    } else {
-      window.scrollTo(0, 0);
-    }
-    if (
-      prevProps.currentPage !== props.currentPage ||
-      prevProps.valueInput !== props.valueInput
-    ) {
-      props.loading(true);
-      return async function response() {
-        try {
-          const response = await axios.get(
-            `${BASE_URL}${MY_API_PIXABAY_KEY}&q=${props.valueInput}&image_type=photo&orientation=horizontal&safesearch=true&page=${props.currentPage}&per_page=${quantityObjects}`
-          );
-          const updateHits = response.data.hits.map(e => {
-            return {
-              id: e.id,
-              tags: e.tags,
-              previewURL: e.previewURL,
-              largeImageURL: e.largeImageURL,
-            };
-          });
-          if (prevProps.valueInput === props.valueInput) {
-            await setUpLoadImages(prevUpLoadImages => [
-              ...prevUpLoadImages,
-              ...updateHits,
-            ]);
-            props.handleUpLoadDataTotalhits(response.data.totalHits);
-          } else {
-            setUpLoadImages(updateHits);
-            props.handleUpLoadDataTotalhits(response.data.totalHits);
-          }
-        } catch (error) {
-          return error;
-        } finally {
-          props.loading(false);
-        }
-      };
-    }
-    props.handleUpLoadImages(upLoadImages);
-  }, [props]);
+export default function ImageGalleryItem({ upLoadImages, openModal }) {
+  if (upLoadImages === '') {
+    return;
+  }
 
   return upLoadImages.map(e => (
     <li key={e.id} className={css.ImageGalleryItem}>
@@ -106,14 +12,13 @@ export default function ImageGalleryItem(props) {
         src={e.previewURL}
         alt={e.tags}
         className={css.ImageGalleryItemImage}
-        onClick={props.openModal}
+        onClick={openModal}
       />
     </li>
   ));
 }
 
 ImageGalleryItem.propTypes = {
-  handleUpLoadImages: PropTypes.func.isRequired,
-  loading: PropTypes.func.isRequired,
-  openModal: PropTypes.func.isRequired,
+  upLoadImages: PropTypes.oneOfType([PropTypes.array, PropTypes.string])
+    .isRequired,
 };
